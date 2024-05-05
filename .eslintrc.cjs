1dc22150540f2e14155ade2a9516c1782f3b04c2
Danh-m1 <danh2k@nexondv.com>
2024-05-05 12:23:11 +0700

    setup: eslint import rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,27 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  plugins: ['import'],
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:import/typescript',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
